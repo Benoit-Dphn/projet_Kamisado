d32ca3056af2994eec961eff7aea4a19f8afb2aa
Benoit-Dphn <dauphin.benoit.fr@gmail.com>
2026-04-27 13:54:15 +0200

    added limited depht and iterative deepening to negamax

--- AI.py
+++ AI.py
@@ -1,18 +1,38 @@
 from utile import gameOver, get_legal_moves, apply
 from evaluation import evaluation_kamisado
+from collections import defaultdict
+import time
 
+def negamaxWithPruningIterativeDeepening(state, player, timeout=2.8):
+	cache = defaultdict(lambda : 0)
+	def cachedNegamaxWithPruningLimitedDepth(state, player, depth, alpha=float('-inf'), beta=float('inf')):
+		over = gameOver(state)
+		if over or depth == 0:
+			res = -evaluation_kamisado(state, player), None, over
 
-def negamaxWithPruning(state, player, alpha=float("-inf"), beta=float("inf")):
-    if gameOver(state):
-        return -evaluation_kamisado(state, player), None
+		else:
+			theValue, theMove, theOver = float('-inf'), None, True
+			possibilities = [(move, apply(state, move)) for move in get_legal_moves(state)]
+			possibilities.sort(key=lambda poss: cache[tuple(poss[1])])
+			for move, successor in reversed(possibilities):
+				value, _, over = cachedNegamaxWithPruningLimitedDepth(successor, player%2+1, depth-1, -beta, -alpha)
+				theOver = theOver and over
+				if value > theValue:
+					theValue, theMove = value, move
+				alpha = max(alpha, theValue)
+				if alpha >= beta:
+					break
+			res = -theValue, theMove, theOver
+		cache[tuple(state)] = res[0]
+		return res
 
-    theValue, theMove = float("-inf"), None
-    for move in get_legal_moves(state):
-        successor = apply(state, move)
-        value, _ = negamaxWithPruning(successor, player % 2 + 1, -beta, -alpha)
-        if value > theValue:
-            theValue, theMove = value, move
-        alpha = max(alpha, theValue)
-        if alpha >= beta:
-            break
-    return -theValue, theMove
+	value, move = 0, None
+	depth = 1
+	start = time.time()
+	over = False
+	while value > -9 and time.time() - start < timeout and not over:
+		value, move, over = cachedNegamaxWithPruningLimitedDepth(state, player, depth)
+		depth += 1
+
+	print('depth =', depth)
+	return value, move
